70194a85c29d4b440b454dc607da5b2cdb2fe1d6
Anton Voznia <avoznia@azul.com>
2026-04-13 17:00:44 +0200

    Remove bootclass and not used jars

--- run-reproducer.sh
+++ run-reproducer.sh
@@ -6,34 +6,25 @@ JAVAC_BIN="${JAVA_HOME_IN_USE}/bin/javac"
 JAVA_BIN="${JAVA_HOME_IN_USE}/bin/java"
 JAR_BIN="${JAVA_HOME_IN_USE}/bin/jar"
 
-TRIGGER_JAR="trigger-fs.jar"
 REPRO_JAR="repro-main.jar"
 RECORD_CONFIG="fs-repro.aotconfig"
 AOT_CACHE="fs-repro.aot"
 
-BASE_OPTS=(
-  -Xbootclasspath/a:"${TRIGGER_JAR}"
-  -Djava.nio.file.spi.DefaultFileSystemProvider=TriggerFsProvider
-  --add-opens=java.base/sun.util.locale.provider=ALL-UNNAMED
-  -XX:+AOTClassLinking
-)
 
 
 rm -f \
   "Repro.class" \
   "TriggerFsProvider.class" \
-  "${TRIGGER_JAR}" \
   "${REPRO_JAR}" \
   "${RECORD_CONFIG}" \
   "${AOT_CACHE}"
 
 "${JAVAC_BIN}" -J-Xshare:off Reproducer.java TriggerFsProvider.java
 
-"${JAR_BIN}" -J-Xshare:off --create --file "${TRIGGER_JAR}" TriggerFsProvider.class
-"${JAR_BIN}" -J-Xshare:off --create --file "${REPRO_JAR}" --main-class Reproducer Reproducer.class
+"${JAR_BIN}" -J-Xshare:off --create --file "${REPRO_JAR}" --main-class Reproducer -C . TriggerFsProvider.class -C . Reproducer.class
 
 "${JAVA_BIN}" \
-  "${BASE_OPTS[@]}" \
+  -Djava.nio.file.spi.DefaultFileSystemProvider=TriggerFsProvider \
   -XX:AOTMode=record \
   -XX:AOTConfiguration="${RECORD_CONFIG}" \
   "-Xlog:aot*=debug" \
@@ -42,7 +33,7 @@ rm -f \
 
 
 "${JAVA_BIN}" \
-  "${BASE_OPTS[@]}" \
+  -Djava.nio.file.spi.DefaultFileSystemProvider=TriggerFsProvider \
   -XX:AOTMode=create \
   -XX:AOTConfiguration="${RECORD_CONFIG}" \
   -XX:AOTCache="${AOT_CACHE}" \
